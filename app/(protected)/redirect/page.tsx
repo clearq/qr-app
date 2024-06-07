a38a098da44c1a4b1e203fe5800609c166569793
JawdatClearQ <you@example.com>
2024-06-07 14:04:44 +0200

    redirect page added

---
+++ app/(protected)/redirect/page.tsx
@@ -0,0 +1,10 @@
+import RedirectForm from "@/components/redirect-form";
+import React from "react";
+
+export default function RedirectPage() {
+  return (
+    <>
+      <RedirectForm url=""/>
+    </>
+  );
+}
---
+++ components/redirect-form.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { useEffect, useState } from 'react';
+import { ParsedUrlQuery } from 'querystring';
+import { Progress } from '@/components/ui/progress';
+
+interface RedirectProps {
+  url: string;
+}
+
+interface Params extends ParsedUrlQuery {
+  qrId: string;
+}
+
+const RedirectForm = ({ url }: RedirectProps) => {
+  const [countdown, setCountdown] = useState(5);
+  const [progress, setProgress] = useState(43)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(prevCountdown => prevCountdown - 1);
+    }, 1000);
+
+    const timeout = setTimeout(() => {
+      window.location.href = url;
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeout);
+    };
+  }, [url]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(95), 4000)
+    return () => clearTimeout(timer)
+  }, [])
+
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1>Redirecting in {countdown} seconds...</h1>
+      <Progress value={progress} className="w-[60%]" />
+    </div>
+  );
+};
+
+export default RedirectForm;
