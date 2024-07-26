c7c6e73ea608ac8ee2f565bd662458154bef4e3b
JawdatClearQ <you@example.com>
2024-07-26 16:31:42 +0200

    fixed analytics with customerId

--- components/qr-silngel-component.tsx
+++ components/qr-silngel-component.tsx
@@ -129,8 +129,8 @@ export const QrSingelComponent = ({ user }: Props) => {
 
   const copyUrlToClipboard = () => {
     //@ts-ignore
-    // const url = `https://qrgen.clearq.se/redirect?id=${qrcodeData?.id}`;
-    const url = `localhost:3000/redirect?id=${qrcodeData?.id}&type=qr`;
+    const url = `https://qrgen.clearq.se/redirect?id=${qrcodeData?.id}`;
+    // const url = `localhost:3000/redirect?id=${qrcodeData?.id}&type=qr`;
     navigator.clipboard
       .writeText(url)
       .then(() => {
